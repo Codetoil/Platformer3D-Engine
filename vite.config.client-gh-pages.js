cb1eb2c1447577f91149fc29f0a16c243dabb8eb
Anthony Michalek <ianthisawesomee@gmail.com>
2025-05-07 21:53:00 -0400

    Update vite.config.client-gh-pages.js

--- vite.config.client-gh-pages.js
+++ vite.config.client-gh-pages.js
@@ -12,7 +12,7 @@ export default defineConfig({
 			}
 		},
 	},
-	base: "https://game3d.codetoil.io",
+	base: "https://platformer3d-engine.codetoil.io",
 	server: {
 		// vite server configs, for details see [vite doc](https://vitejs.dev/config/#server-host)
 		port: 3000
